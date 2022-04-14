cc5638ae7dd7da140c72f72c0808457d5e8c89a8
Naithcots <sebastian.stochmial2000@gmail.com>
2022-04-14 13:06:54 +0200

    Add random sketch colors and reset button

--- app.js
+++ app.js
@@ -1,18 +1,67 @@
 const sketchContainer = document.querySelector(".sketch-container");
+const resetBtn = document.querySelector("#reset");
+let brightness = 1;
 
-const generateSketch = () => {
-  // 16 x 16
-  for (let i = 0; i < 16 ** 2; i++) {
+const randomRGB = () => {
+  let rgb = [];
+  for (let i = 0; i < 3; i++) {
+    const randomNum = Math.random() * 255;
+    rgb.push(randomNum);
+  }
+  return `rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]})`;
+};
+
+const colorizeElement = (e) => {
+  if (e.buttons === 1)
+    e.target.style.cssText = `background-color: ${randomRGB()}; filter: brightness(${brightness});`;
+  brightness >= 0.1 + Number.EPSILON ? (brightness -= 0.1) : (brightness = 0);
+};
+
+const assignListeners = () => {
+  [...sketchContainer.children].forEach((box) => {
+    box.addEventListener("mouseover", colorizeElement, { once: true });
+  });
+};
+
+const removeListeners = () => {
+  [...sketchContainer.children].forEach((box) => {
+    box.removeEventListener("mouseover", undefined);
+  });
+};
+
+const generateSketch = (dim) => {
+  brightness = 1;
+
+  sketchContainer.style.cssText = `
+        grid-template-columns: repeat(${dim}, 1fr);
+        grid-template-rows: repeat(${dim}, 1fr);
+    `;
+
+  for (let i = 0; i < dim ** 2; i++) {
     const box = document.createElement("div");
     box.classList.add("box");
     sketchContainer.appendChild(box);
   }
+
+  assignListeners();
 };
 
-generateSketch();
+const resetSketch = () => {
+  removeListeners();
+  sketchContainer.innerHTML = "";
+};
 
-[...sketchContainer.children].forEach((box) => {
-  box.addEventListener("mouseover", (e) => {
-    e.target.style.cssText = "background-color: black;";
-  });
+resetBtn.addEventListener("click", () => {
+  let gridCols = null;
+
+  gridCols = prompt("Enter grid dims (max: 128)", 16);
+
+  while (typeof gridCols !== "number" && gridCols < 2 && gridCols > 100) {
+    gridCols = prompt("Enter grid dims (max: 100)", 16);
+  }
+
+  resetSketch();
+  generateSketch(gridCols);
 });
+
+generateSketch(16);
